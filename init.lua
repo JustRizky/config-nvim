7f47dcb139a19289d19e3af81f7500dbd078319c
Muhammad Rizky Kurniawan Lasim <rizky@Muhammads-MacBook-Air.local>
2023-04-09 06:33:43 +0700

    config-nvim-1

---
+++ init.lua
@@ -0,0 +1,320 @@
+vim.g.mapleader = " "
+
+require("packer").startup(function(use)
+	use { "wbthomason/packer.nvim" }
+	use { "rebelot/kanagawa.nvim" }
+	use { "christoomey/vim-tmux-navigator" }
+	use { "szw/vim-maximizer" }
+	use { "tpope/vim-surround" }
+	use { "inkarkat/vim-ReplaceWithRegister" }
+	use { "nvim-tree/nvim-tree.lua" }
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		 requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use {
+	  'nvim-lualine/lualine.nvim',
+	   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+	use { "fatih/vim-go" }
+	use ({
+		"glepnir/lspsaga.nvim",
+   		branch = "main",
+	})
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+  		branch = 'v1.x',
+  		requires = {
+		{'neovim/nvim-lspconfig'},             -- Required
+		{'williamboman/mason.nvim'},           -- Optional
+		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+		{'hrsh7th/nvim-cmp'},         -- Required
+		{'hrsh7th/cmp-nvim-lsp'},     -- Required
+		{'hrsh7th/cmp-buffer'},       -- Optional
+		{'hrsh7th/cmp-path'},         -- Optional
+		{'saadparwaiz1/cmp_luasnip'}, -- Optional
+		{'hrsh7th/cmp-nvim-lua'},     -- Optional
+		{'L3MON4D3/LuaSnip'},             -- Required
+		{'rafamadriz/friendly-snippets'}, -- Optional
+  	},
+	use {"akinsho/toggleterm.nvim", tag = '*' },
+	use "jhlgns/naysayer88.vim",
+	use "terrortylor/nvim-comment",
+	use "CreaturePhil/vim-handmade-hero",
+	use "lewis6991/gitsigns.nvim",
+	use "windwp/nvim-autopairs",
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })
+}
+end)
+
+-- TREESITTER
+require'nvim-treesitter.configs'.setup {
+	ensure_installed = {"cpp", "c", "lua", "vim", "go", "javascript", "typescript", "rust"},
+	highlight = {
+		enable = true,
+	}
+}
+
+-- LUALINE
+require("lualine").setup{
+	options = {
+		icons_enabled = false,
+		theme = "onedark",
+		component_separators = "|",
+		section_separators = "",
+	},
+}
+
+-- LSP
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+	"tsserver",
+	"clangd",
+	"gopls",
+	"eslint",
+	"rust_analyzer",
+})
+
+lsp.set_preferences({
+	sign_icons = {}
+})
+
+lsp.on_attach(function(client, bufnr)
+	local opts = {buffer = bufnr, remap = false}
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+end)
+
+lsp.setup()
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+		signs = false,
+		virtual_text = true,
+		underline = false,
+	}
+)
+
+-- LSPSAGA
+local saga_status, saga = pcall(require, "lspsaga")
+if not saga_status then
+  return
+end
+
+saga.setup({
+  -- keybinds for navigation in lspsaga window
+  scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+  -- use enter to open file with definition preview
+  definition = {
+    edit = "<CR>",
+  },
+  ui = {
+    colors = {
+      normal_bg = "#022746",
+    },
+  },
+})
+
+-- COMMENT
+require("nvim_comment").setup({
+	operator_mapping = "<leader>/"
+})
+
+-- AUTOPAIRS
+local autopairs_setup, autopairs = pcall(require, "nvim-autopairs")
+if not autopairs_setup then
+  return
+end
+
+-- configure autopairs
+autopairs.setup({
+  check_ts = true, -- enable treesitter
+  ts_config = {
+    lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+    javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+    java = false, -- don't check treesitter on java
+  },
+})
+
+-- import nvim-autopairs completion functionality safely
+local cmp_autopairs_setup, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+if not cmp_autopairs_setup then
+  return
+end
+
+-- import nvim-cmp plugin safely (completions plugin)
+local cmp_setup, cmp = pcall(require, "cmp")
+if not cmp_setup then
+  return
+end
+
+-- make autopairs and completion work together
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+-- GITSIGNS
+local setup, gitsigns = pcall(require, "gitsigns")
+if not setup then
+  return
+end
+
+-- configure/enable gitsigns
+gitsigns.setup()
+
+-- NVIM-TREE
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+  return
+end
+
+-- recommended settings from nvim-tree documentation
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- change color for arrows in tree to light blue
+vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+-- configure nvim-tree
+nvimtree.setup({
+  -- change folder arrow icons
+  renderer = {
+    icons = {
+      glyphs = {
+        folder = {
+          arrow_closed = "", -- arrow when folder is closed
+          arrow_open = "", -- arrow when folder is open
+        },
+      },
+    },
+  },
+  -- disable window_picker for
+  -- explorer to work well with
+  -- window splits
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = false,
+      },
+    },
+  },
+  -- 	git = {
+  -- 		ignore = false,
+  -- 	},
+})
+
+-- open nvim-tree on setup
+
+local function open_nvim_tree(data)
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+-- TERMINAL SETUP
+require("toggleterm").setup{
+	direction = "horizontal",
+	size = 15,
+	open_mapping = [[<M-j>]]
+}
+
+-- COLORSCHEME
+vim.cmd("colorscheme kanagawa")
+
+vim.cmd([[
+	augroup CustomCommentCollor
+		autocmd!
+		autocmd VimEnter * hi Comment guifg=#2ea542
+	augroup END
+]])
+
+vim.cmd([[
+	augroup CustomHI
+		autocmd!
+		autocmd VimEnter * NoMatchParen 
+	augroup END
+]])
+
+vim.o.background = "dark"
+
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
+vim.keymap.set("n", "x", '"_x')
+vim.keymap.set("n", "<leader>+", "<C-a>")
+vim.keymap.set("n", "<leader>-", "<C-x>")
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
+-- PLUGIN KEYBIND
+-- vim-maximizer
+vim.keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+
+-- nvim-tree
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+
+-- telescope
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+-- telescope git commands (not on youtube nvim video)
+vim.keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+vim.keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+vim.keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+
+-- restart lsp server (not on youtube nvim video)
+vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+
+
+vim.opt.guicursor = "i:block"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.swapfile = false
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.backspace = "indent,eol,start"
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.iskeyword:append("-")
+
+vim.o.hlsearch = true
+vim.o.mouse = 'a'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+vim.o.termguicolor = true
